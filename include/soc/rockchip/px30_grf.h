8c626c6ca7009d323e93e62ea07d63890928e839
Cosmin Tanislav <demonsingur@gmail.com>
2022-01-04 22:37:28 +0200

    devfreq: event: add PX30 DFI driver

---
+++ include/soc/rockchip/px30_grf.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Rockchip General Register Files definitions
+ *
+ * Copyright (c) 2018, Collabora Ltd.
+ * Author: Enric Balletbo i Serra <enric.balletbo@collabora.com>
+ */
+
+#ifndef __SOC_PX30_GRF_H
+#define __SOC_PX30_GRF_H
+
+/* PMU GRF Registers */
+#define PX30_PMUGRF_OS_REG2		0x208
+#define PX30_PMUGRF_DDRTYPE_SHIFT	13
+#define PX30_PMUGRF_DDRTYPE_MASK	7
+#define PX30_PMUGRF_DDRTYPE_DDR3	3
+#define PX30_PMUGRF_DDRTYPE_LPDDR2	5
+#define PX30_PMUGRF_DDRTYPE_LPDDR3	6
+#define PX30_PMUGRF_DDRTYPE_LPDDR4	7
+
+#endif
