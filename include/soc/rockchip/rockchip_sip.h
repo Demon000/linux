b691ee9842ad0082b6dae8ef50d87eb7c476cda2
Cosmin Tanislav <demonsingur@gmail.com>
2022-01-05 18:39:52 +0200

    include: soc: rockchip: define ATF share params

--- include/soc/rockchip/rockchip_sip.h
+++ include/soc/rockchip/rockchip_sip.h
@@ -7,6 +7,7 @@
 #define __SOC_ROCKCHIP_SIP_H
 
 #define ROCKCHIP_SIP_DRAM_FREQ			0x82000008
+#define ROCKCHIP_SIP_SHARE_MEM			0x82000009
 #define ROCKCHIP_SIP_CONFIG_DRAM_INIT		0x00
 #define ROCKCHIP_SIP_CONFIG_DRAM_SET_RATE	0x01
 #define ROCKCHIP_SIP_CONFIG_DRAM_ROUND_RATE	0x02
@@ -16,5 +17,28 @@
 #define ROCKCHIP_SIP_CONFIG_DRAM_CLR_IRQ	0x06
 #define ROCKCHIP_SIP_CONFIG_DRAM_SET_PARAM	0x07
 #define ROCKCHIP_SIP_CONFIG_DRAM_SET_ODT_PD	0x08
+
+#define SHARE_PAGE_TYPE_DDR	2
+
+struct share_params {
+	u32 hz;
+	u32 lcdc_type;
+	u32 vop;
+	u32 vop_dclk_mode;
+	u32 sr_idle_en;
+	u32 addr_mcu_el3;
+	/*
+	 * 1: need to wait flag1
+	 * 0: never wait flag1
+	 */
+	u32 wait_flag1;
+	/*
+	 * 1: need to wait flag1
+	 * 0: never wait flag1
+	 */
+	u32 wait_flag0;
+	u32 complete_hwirq;
+	 /* if need, add parameter after */
+};
 
 #endif
