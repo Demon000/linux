3189c0c7998ecbf8635a2b0381968754f6282f50
Cosmin Tanislav <cosmin.tanislav@analog.com>
2023-03-06 12:24:33 +0200

    staging: media: max96717: add ctrls from subdevice

--- drivers/staging/media/max96717/max96717.c
+++ drivers/staging/media/max96717/max96717.c
@@ -308,6 +308,15 @@ static int max96717_notify_bound(struct v4l2_async_notifier *notifier,
 		goto error_remove_link;
 	}
 
+	/* Add controls from the subdevice */
+	ret = v4l2_ctrl_add_handler(&priv->ctrl_handler,
+				    priv->sensor->ctrl_handler,
+				    NULL, true);
+	if (ret) {
+		dev_err(priv->dev, "Failed to add subdevice controls %d\n", ret);
+		goto error_remove_link;
+	}
+
 	return 0;
 
 error_remove_link:
