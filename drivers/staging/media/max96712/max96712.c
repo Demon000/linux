f32ec744e26f56e1084b5c757708cb2cd1b0f34f
Cosmin Tanislav <demonsingur@gmail.com>
2023-03-16 12:57:11 +0200

    staging: media: max96712: backport

--- drivers/staging/media/max96712/max96712.c
+++ drivers/staging/media/max96712/max96712.c
@@ -30,6 +30,10 @@
 #define v4l2_subdev_state v4l2_subdev_pad_config
 #define v4l2_subdev_alloc_state v4l2_subdev_alloc_pad_config
 #define v4l2_subdev_free_state v4l2_subdev_free_pad_config
+#undef v4l2_async_notifier_add_fwnode_subdev
+#define v4l2_async_notifier_add_fwnode_subdev(__notifier, __fwnode, __type) \
+((__type *)__v4l2_async_notifier_add_fwnode_subdev(__notifier, __fwnode,    \
+                           sizeof(__type)))
 
 struct max96712_asd {
 	struct v4l2_async_subdev base;
