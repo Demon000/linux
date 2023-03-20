451de209b54f3b96ccff3af0e89404f86f10327a
Cosmin Tanislav <demonsingur@gmail.com>
2023-03-20 16:49:13 +0200

    staging: media: max96717: enable RCLKM output on GPIO 4

--- drivers/staging/media/max96717/max96717.c
+++ drivers/staging/media/max96717/max96717.c
@@ -572,12 +572,14 @@ static int max96717_init(struct max96717_priv *priv)
 	 * Enable RCLK output at fastest slew rate on GPIO 4.
 	 */
 	max96717_update_bits(priv, 0x6, BIT(5), BIT(5));
+	max96717_update_bits(priv, 0x3, GENMASK(1, 0), 0b00);
 	max96717_update_bits(priv, 0x570, GENMASK(5, 4),
 						 FIELD_PREP(GENMASK(5, 4), 0b00));
 
 	max96717_update_bits(priv, 0x3f1, BIT(7), BIT(7));
 	max96717_update_bits(priv, 0x3f1, BIT(0), BIT(0));
-	max96717_update_bits(priv, 0x3f1, GENMASK(5, 1), 0x4);
+	max96717_update_bits(priv, 0x3f1, GENMASK(5, 1),
+						 FIELD_PREP(GENMASK(5, 1), 0x4));
 
 	return 0;
 }
