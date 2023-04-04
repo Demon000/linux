1a863ee0a1df798e71ddbf966e54586ebc0acbd3
Cosmin Tanislav <demonsingur@gmail.com>
2023-04-04 15:27:09 +0300

    Revert "Revert "staging: media: max96712: take into account all subdev status before enabling mipi""
    This reverts commit a86ad2a97cb857e5d7b6fcaa450af6c7046e549d.

--- drivers/staging/media/max96712/max96712.c
+++ drivers/staging/media/max96712/max96712.c
@@ -63,6 +63,8 @@ struct max96712_subdev_priv {
 	struct media_pad pads[MAX96712_PAD_NUM];
 
 	struct v4l2_fwnode_bus_mipi_csi2 mipi;
+
+	bool active;
 };
 
 struct max96712_priv {
@@ -77,6 +79,8 @@ struct max96712_priv {
 
 	unsigned int lane_config;
 	bool skip_subdev_s_stream;
+	struct mutex lock;
+	bool active;
 
 	struct max96712_subdev_priv sd_privs[MAX96712_SUBDEVS_NUM];
 };
@@ -230,9 +234,20 @@ error:
 	return ret;
 }
 
-static void max96712_mipi_enable(struct max96712_priv *priv, bool enable)
+static void __max96712_mipi_update(struct max96712_priv *priv)
 {
-	/* TODO: use a counter to handle multiple active ports. */
+	struct max96712_subdev_priv *sd_priv;
+	bool enable = 0;
+
+	for_each_subdev(priv, sd_priv)
+		if (sd_priv->active)
+			enable = 1;
+
+	if (enable == priv->active)
+		return;
+
+	priv->active = enable;
+
 	if (enable) {
 		max96712_update_bits(priv, 0x40b, 0x02, 0x02);
 		max96712_update_bits(priv, 0x8a0, 0x80, 0x80);
@@ -240,6 +255,23 @@ static void max96712_mipi_enable(struct max96712_priv *priv, bool enable)
 		max96712_update_bits(priv, 0x8a0, 0x80, 0x00);
 		max96712_update_bits(priv, 0x40b, 0x02, 0x00);
 	}
+}
+
+static void max96712_mipi_enable(struct max96712_subdev_priv *sd_priv, bool enable)
+{
+	struct max96712_priv *priv = sd_priv->priv;
+
+	mutex_lock(&priv->lock);
+
+	if (sd_priv->active == enable)
+		goto exit;
+
+	sd_priv->active = enable;
+
+exit:
+	__max96712_mipi_update(priv);
+
+	mutex_unlock(&priv->lock);
 }
 
 static void max96712_init_phy(struct max96712_subdev_priv *sd_priv)
@@ -313,7 +345,7 @@ static void max96712_init(struct max96712_priv *priv)
 {
 	struct max96712_subdev_priv *sd_priv;
 
-	max96712_mipi_enable(priv, false);
+	__max96712_mipi_update(priv);
 
 	/* Select 2x4 or 4x2 mode. */
 	max96712_update_bits(priv, 0x8a0, 0x1f, BIT(priv->lane_config));
@@ -473,7 +505,7 @@ static int max96712_s_stream(struct v4l2_subdev *sd, int enable)
 	}
 
 skip:
-	max96712_mipi_enable(priv, enable);
+	max96712_mipi_enable(sd_priv, enable);
 
 	return 0;
 }
