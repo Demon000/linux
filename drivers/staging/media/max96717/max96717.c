77bb89889f109ba1365849d80b1115afcc6a997d
Cosmin Tanislav <demonsingur@gmail.com>
2023-04-14 16:15:10 +0300

    staging: media: max96717: fix indent

--- drivers/staging/media/max96717/max96717.c
+++ drivers/staging/media/max96717/max96717.c
@@ -538,12 +538,12 @@ static void max96717_init(struct max96717_priv *priv)
 	max96717_update_bits(priv, 0x6, BIT(5), BIT(5));
 	max96717_update_bits(priv, 0x3, GENMASK(1, 0), 0b00);
 	max96717_update_bits(priv, 0x570, GENMASK(5, 4),
-						 FIELD_PREP(GENMASK(5, 4), 0b00));
+			     FIELD_PREP(GENMASK(5, 4), 0b00));
 
 	max96717_update_bits(priv, 0x3f1, BIT(7), BIT(7));
 	max96717_update_bits(priv, 0x3f1, BIT(0), BIT(0));
 	max96717_update_bits(priv, 0x3f1, GENMASK(5, 1),
-						 FIELD_PREP(GENMASK(5, 1), 0x4));
+			     FIELD_PREP(GENMASK(5, 1), 0x4));
 
 	msleep(1000);
 
