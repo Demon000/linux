efeae620ae8b009084c0fb201c21cf6a79be8a08
Cosmin Tanislav <demonsingur@gmail.com>
2023-05-19 11:46:06 +0300

    staging: media: maxim: des: fix bad usage of phys number

--- drivers/staging/media/maxim/max_des.h
+++ drivers/staging/media/maxim/max_des.h
@@ -78,7 +78,7 @@ struct max_des_priv {
 	bool active;
 
 	struct max_des_phy phys[MAX_DES_PHYS_NUM];
-	struct max_des_pipe pipes[MAX_DES_PHYS_NUM];
+	struct max_des_pipe pipes[MAX_DES_PIPES_NUM];
 	struct max_des_subdev_priv *sd_privs;
 };
 
