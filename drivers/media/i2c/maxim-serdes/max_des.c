5dba9421ba75a2f26f53e993302277f8f9278557
Cosmin Tanislav <demonsingur@gmail.com>
2024-01-08 15:03:40 +0200

    media: i2c: maxim-serdes: register unattached phys too

--- drivers/media/i2c/maxim-serdes/max_des.c
+++ drivers/media/i2c/maxim-serdes/max_des.c
@@ -99,7 +99,7 @@ static int max_des_register_async_v4l2(struct max_des_priv *priv)
 
 		comp = &priv->phys_comp[i];
 
-		ret = max_des_phy_register_v4l2_sd(priv, phy, comp, attach_notifier);
+		ret = max_des_phy_register_v4l2_sd(priv, phy, comp, attach_notifier, NULL);
 		if (ret)
 			return ret;
 
@@ -144,13 +144,6 @@ static int max_des_register_pipe_phy_xbar(struct max_des_priv *priv,
 	/* Create xbar->PHY links. */
 	offset = 0;
 	for (i = 0; i < des->ops->num_phys; i++) {
-		struct max_des_phy *phy = &des->phys[i];
-
-		if (!phy->enabled) {
-			offset++;
-			continue;
-		}
-
 		other_comp = &priv->phys_comp[i];
 
 		ret = max_components_link(comp, offset, other_comp, 0);
@@ -235,6 +228,19 @@ int max_des_register_v4l2(struct max_des_priv *priv, struct v4l2_device *v4l2_de
 	if (priv->num_bound_phys != des->num_enabled_phys)
 		return 0;
 
+	for (i = 0; i < des->ops->num_phys; i++) {
+		struct max_des_phy *phy = &des->phys[i];
+
+		if (phy->enabled)
+			continue;
+
+		comp = &priv->phys_comp[i];
+
+		ret = max_des_phy_register_v4l2_sd(priv, phy, comp, false, v4l2_dev);
+		if (ret)
+			return ret;
+	}
+
 	for (i = 0; i < des->ops->num_pipes; i++) {
 		struct max_des_pipe *pipe = &des->pipes[i];
 
@@ -299,6 +305,17 @@ void max_des_unregister_v4l2(struct max_des_priv *priv)
 		comp = &priv->pipes_comp[i];
 
 		max_des_pipe_unregister_v4l2_sd(priv, comp);
+	}
+
+	for (i = 0; i < des->ops->num_phys; i++) {
+		struct max_des_phy *phy = &des->phys[i];
+
+		if (phy->enabled)
+			continue;
+
+		comp = &priv->phys_comp[i];
+
+		max_des_phy_unregister_v4l2_sd(priv, comp);
 	}
 }
 
