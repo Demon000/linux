4a91a306e07fa3083fb7a7f0c2a4d4659ac571e9
Cosmin Tanislav <demonsingur@gmail.com>
2024-02-29 15:59:35 +0200

    media: i2c: maxim-serdes: fix enabling remaps

--- drivers/media/i2c/maxim-serdes/max96724.c
+++ drivers/media/i2c/maxim-serdes/max96724.c
@@ -453,12 +453,12 @@ static int max96724_set_pipe_remap_enable(struct max_des *des,
 {
 	struct max96724_priv *priv = des_to_priv(des);
 	unsigned int index = pipe->index;
-	unsigned int reg, val;
+	unsigned int reg, mask;
 
 	/* Enable remap. */
 	reg = 0x90b + 0x40 * index + i / 8;
-	val = BIT(i % 8);
-	return max96724_update_bits(priv, reg, val, val);
+	mask = BIT(i % 8);
+	return max96724_update_bits(priv, reg, mask, enable ? mask : 0);
 }
 
 static int max96724_set_pipe_link(struct max_des *des, struct max_des_pipe *pipe,
