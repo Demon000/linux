e42f722fd08dd439d93cf55d1799220a3cd3bb6e
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-13 22:15:21 +0200

    media: i2c: maxim-serdes: use common function for getting the updated streams mask

--- drivers/media/i2c/maxim-serdes/max_serdes.h
+++ drivers/media/i2c/maxim-serdes/max_serdes.h
@@ -69,4 +69,11 @@ int max_xlate_enable_disable_streams(struct max_source *sources,
 				     u32 sink_pad_start, u32 num_sink_pads,
 				     bool enable);
 
+int max_get_streams_masks(struct device *dev,
+			  const struct v4l2_subdev_krouting *routing,
+			  u32 pad, u64 updated_streams_mask,
+			  u32 num_pads, u32 sink_pad_start, u32 num_sink_pads,
+			  u64 *old_streams_masks, u64 **new_streams_masks,
+			  bool enable);
+
 #endif // MAX_SERDES_H
