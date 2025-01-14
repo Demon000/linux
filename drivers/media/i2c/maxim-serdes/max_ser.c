3fa4757af4f8a217b63a2483357b006bd6f2309d
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-14 15:04:30 +0200

    Revert "fixup! media: i2c: maxim-serdes: do not allocate initial streams_masks"
    This reverts commit 55f1064ecc3af1b39ece4af34e19f8168227f2cb.

--- drivers/media/i2c/maxim-serdes/max_ser.c
+++ drivers/media/i2c/maxim-serdes/max_ser.c
@@ -756,7 +756,6 @@ static int max_ser_update_streams(struct v4l2_subdev *sd,
 
 	if (priv->streams_masks)
 		devm_kfree(priv->dev, priv->streams_masks);
-
 	priv->streams_masks = streams_masks;
 	ser->active = !!streams_masks[pad];
 
