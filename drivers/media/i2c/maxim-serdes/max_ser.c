8d78206e52390f0b70a54ef9a0dc6b3546ff24ec
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-18 16:15:06 +0200

    media: i2c: maxim-serdes: max_serdes: add affected sink pads mask to max_get_streams_masks()

--- drivers/media/i2c/maxim-serdes/max_ser.c
+++ drivers/media/i2c/maxim-serdes/max_ser.c
@@ -739,7 +739,7 @@ static int max_ser_update_streams(struct v4l2_subdev *sd,
 
 	ret = max_get_streams_masks(priv->dev, &state->routing,
 				    pad, updated_streams_mask,
-				    num_pads, 0, ser->ops->num_phys,
+				    num_pads, 0, ser->ops->num_phys, NULL,
 				    priv->streams_masks, &streams_masks, enable);
 	if (ret)
 		return ret;
