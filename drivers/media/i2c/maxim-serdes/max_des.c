e62db9d9917ec67ab17f83e3d8348bb237b44a3e
Cosmin Tanislav <demonsingur@gmail.com>
2025-05-09 23:16:28 +0300

    media: i2c: maxim-serdes: max_des: only require select_link_version for >= 1 versions

--- drivers/media/i2c/maxim-serdes/max_des.c
+++ drivers/media/i2c/maxim-serdes/max_des.c
@@ -2261,7 +2261,7 @@ int max_des_probe(struct i2c_client *client, struct max_des *des)
 		return -EINVAL;
 	}
 
-	if (hweight_long(des->ops->versions) != 1 &&
+	if (hweight_long(des->ops->versions) >= 1 &&
 	    !des->ops->select_link_version) {
 		dev_err(dev, "Multiple version without .select_link_version()\n");
 		return -EINVAL;
