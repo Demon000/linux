bc8d23a081bc6ef450f19957312e71126b075bda
Cosmin Tanislav <demonsingur@gmail.com>
2025-05-20 09:58:40 +0300

    media: i2c: maxim-serdes: remove double checks for source->sd

--- drivers/media/i2c/maxim-serdes/max_des.c
+++ drivers/media/i2c/maxim-serdes/max_des.c
@@ -558,9 +558,6 @@ static int max_des_set_modes(struct max_des_priv *priv,
 		if (!pipe)
 			return -ENOENT;
 
-		if (!source->sd)
-			continue;
-
 		pipe_double_bpps = context->pipes_double_bpps[pipe->index];
 
 		ret = max_ser_set_double_bpps(source->sd, pipe_double_bpps);
@@ -644,9 +641,6 @@ static int max_des_set_pipes_stream_id(struct max_des_priv *priv)
 		pipe = max_des_find_link_pipe(des, link);
 		if (!pipe)
 			return -ENOENT;
-
-		if (!source->sd)
-			continue;
 
 		stream_id = pipe->stream_id;
 
