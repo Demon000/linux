50ae2ed788af11544a3924a8cc3e9a933871cf2f
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-01 01:19:59 +0300

    media: i2c: maxim-serdes: max96717: inline lane polarity val = 0

--- drivers/media/i2c/maxim-serdes/max96717.c
+++ drivers/media/i2c/maxim-serdes/max96717.c
@@ -964,8 +964,7 @@ static int max96717_init_phy(struct max_ser *ser,
 		return ret;
 
 	/* Configure lane polarity. */
-	val = 0;
-	for (i = 0; i < num_data_lanes; i++)
+	for (i = 0, val = 0; i < num_data_lanes; i++)
 		if (phy->mipi.lane_polarities[i + 1])
 			val |= BIT(i);
 
