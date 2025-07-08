e93c35a7ae4f2a4efa076d0411a97579a5b27673
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-08 21:19:36 +0300

    media: i2c: maxim-serdes: max96724: import MAXIM_SERDES namespace

--- drivers/media/i2c/maxim-serdes/max96724.c
+++ drivers/media/i2c/maxim-serdes/max96724.c
@@ -1177,6 +1177,7 @@ static struct i2c_driver max96724_i2c_driver = {
 
 module_i2c_driver(max96724_i2c_driver);
 
+MODULE_IMPORT_NS("MAX_SERDES");
 MODULE_DESCRIPTION("Maxim MAX96724 Quad GMSL2 Deserializer Driver");
 MODULE_AUTHOR("Cosmin Tanislav <cosmin.tanislav@analog.com>");
 MODULE_LICENSE("GPL");
