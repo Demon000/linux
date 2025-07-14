a2d59c4856cf710ebebb2443f3eadb38f565993b
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-14 15:13:18 +0300

    media: i2c: maxim-serdes: max_des: limit default routing to a single route

--- drivers/media/i2c/maxim-serdes/max_des.c
+++ drivers/media/i2c/maxim-serdes/max_des.c
@@ -2483,6 +2483,14 @@ static int max_des_init_state(struct v4l2_subdev *sd,
 			.flags = V4L2_SUBDEV_ROUTE_FL_ACTIVE,
 		};
 		stream++;
+
+		/*
+		 * The Streams API is an experimental feature.
+		 * If multiple routes are provided here, userspace will not be
+		 * able to configure them unless the Streams API is enabled.
+		 * Provide a single route until it is enabled.
+		 */
+		break;
 	}
 
 	return __max_des_set_routing(sd, state, &routing);
