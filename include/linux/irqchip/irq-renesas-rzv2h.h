2259956c3e9a539985f235fc9edb38aa5bf6be39
Cosmin Tanislav <cosmin-gabriel.tanislav.xa@renesas.com>
2025-12-23 17:55:13 +0200

    irqchip: irq-renesas-rzv2h: add rzv2h_icu_register_dma_req_new()
    Change-Id: Ice4e02c9a5b357e7b16f6b14b2a942c98f18b0d3

--- include/linux/irqchip/irq-renesas-rzv2h.h
+++ include/linux/irqchip/irq-renesas-rzv2h.h
@@ -15,9 +15,13 @@
 #ifdef CONFIG_RENESAS_RZV2H_ICU
 void rzv2h_icu_register_dma_req(struct platform_device *icu_dev, u8 dmac_index, u8 dmac_channel,
 				u16 req_no);
+void rzv2h_icu_register_dma_req_new(struct platform_device *icu_dev, u8 dmac_index, u8 dmac_channel,
+				    int req_no);
 #else
 static inline void rzv2h_icu_register_dma_req(struct platform_device *icu_dev, u8 dmac_index,
 					      u8 dmac_channel, u16 req_no) { }
+static inline void rzv2h_icu_register_dma_req_new(struct platform_device *icu_dev, u8 dmac_index,
+						  u8 dmac_channel, int req_no) { }
 #endif
 
 #endif /* __LINUX_IRQ_RENESAS_RZV2H */
