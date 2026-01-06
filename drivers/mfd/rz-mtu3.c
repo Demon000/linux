c96447c10269d22d3bfcd10ae021c232e97e87ca
Cosmin Tanislav <cosmin-gabriel.tanislav.xa@renesas.com>
2026-01-06 15:34:34 +0200

    mfd: rz-mtu3: reuse shared_reg_update_bit() in start_stop_ch()
    rz_mtu3_start_stop_ch() performs the same shared register update
    sequence already implemented by rz_mtu3_shared_reg_update_bit().
    
    Reuse the helper instead of open-coding the read-modify-write sequence.
    
    Change-Id: Ieec8990b49a37930c132741888da4a2217bfc885
    Signed-off-by: Cosmin Tanislav <cosmin-gabriel.tanislav.xa@renesas.com>

--- drivers/mfd/rz-mtu3.c
+++ drivers/mfd/rz-mtu3.c
@@ -243,22 +243,8 @@ static u8 rz_mtu3_get_tstr_bit_pos(struct rz_mtu3_channel *ch)
 
 static void rz_mtu3_start_stop_ch(struct rz_mtu3_channel *ch, bool start)
 {
-	struct rz_mtu3_priv *priv = rz_mtu3_ch_to_priv(ch);
-	unsigned long flags, tstr;
-	u16 offset;
-	u8 bitpos;
-
-	offset = rz_mtu3_get_tstr_offset(ch);
-	bitpos = rz_mtu3_get_tstr_bit_pos(ch);
-
-	/* start stop register shared by multiple timer channels */
-	spin_lock_irqsave(&priv->lock, flags);
-
-	tstr = rz_mtu3_shared_reg_read(ch, offset);
-	__assign_bit(bitpos, &tstr, start);
-	rz_mtu3_shared_reg_write(ch, offset, tstr);
-
-	spin_unlock_irqrestore(&priv->lock, flags);
+	rz_mtu3_shared_reg_update_bit(ch, rz_mtu3_get_tstr_offset(ch),
+				      rz_mtu3_get_tstr_bit_pos(ch), start);
 }
 
 bool rz_mtu3_is_enabled(struct rz_mtu3_channel *ch)
