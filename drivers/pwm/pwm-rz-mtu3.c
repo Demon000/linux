22b42fc59f51386f9c7914489f47a156217de302
Cosmin Tanislav <cosmin-gabriel.tanislav.xa@renesas.com>
2026-01-06 15:35:42 +0200

    pwm: rz-mtu3: correctly enable HW channel 4 and 7
    HW channels 4 and 7 require an additional bit to be set in the TOER{A,B}
    registers to enable PWM output.
    
    Add the necessary logic to set these bits when enabling or disabling PWM
    on these channels.
    
    Change-Id: Iea4ba2d94357229f8da62fe23e1f09102e292ff8

--- drivers/pwm/pwm-rz-mtu3.c
+++ drivers/pwm/pwm-rz-mtu3.c
@@ -302,6 +302,35 @@ static void rz_mtu3_pwm_free(struct pwm_chip *chip, struct pwm_device *pwm)
 	mutex_unlock(&rz_mtu3_pwm->lock);
 }
 
+static void rz_mtu3_pwm_set_toer_bit(struct rz_mtu3_pwm_chip *rz_mtu3_pwm,
+				     struct rz_mtu3_pwm_channel *priv,
+				     bool is_primary, bool set)
+{
+	u8 bitpos;
+	u16 reg;
+	u32 ch;
+
+	ch = rz_mtu3_channel_number(rz_mtu3_pwm, priv);
+
+	/*
+	 * HW channels 4 and 7 require an additional register write to enable
+	 * PWM output.
+	 */
+	if (ch == 4)
+		reg = RZ_MTU3_TOERA;
+	else if (ch == 6)
+		reg = RZ_MTU3_TOERB;
+	else
+		return;
+
+	if (is_primary)
+		bitpos = 1;
+	else
+		bitpos = 4;
+
+	rz_mtu3_shared_reg_update_bit(priv->mtu, reg, bitpos, set);
+}
+
 static int rz_mtu3_pwm_enable(struct pwm_chip *chip, struct pwm_device *pwm)
 {
 	struct rz_mtu3_pwm_chip *rz_mtu3_pwm = to_rz_mtu3_pwm_chip(chip);
@@ -319,6 +348,9 @@ static int rz_mtu3_pwm_enable(struct pwm_chip *chip, struct pwm_device *pwm)
 	val = RZ_MTU3_TIOR_OC_IOB_L_COMP_MATCH | RZ_MTU3_TIOR_OC_IOA_H_COMP_MATCH;
 
 	rz_mtu3_8bit_ch_write(priv->mtu, RZ_MTU3_TMDR1, RZ_MTU3_TMDR1_MD_PWMMODE1);
+
+	rz_mtu3_pwm_set_toer_bit(rz_mtu3_pwm, priv, is_primary, true);
+
 	if (is_primary)
 		rz_mtu3_8bit_ch_write(priv->mtu, RZ_MTU3_TIORH, val);
 	else
@@ -347,6 +379,8 @@ static void rz_mtu3_pwm_disable(struct pwm_chip *chip, struct pwm_device *pwm)
 		rz_mtu3_8bit_ch_write(priv->mtu, RZ_MTU3_TIORH, RZ_MTU3_TIOR_OC_RETAIN);
 	else
 		rz_mtu3_8bit_ch_write(priv->mtu, RZ_MTU3_TIORL, RZ_MTU3_TIOR_OC_RETAIN);
+
+	rz_mtu3_pwm_set_toer_bit(rz_mtu3_pwm, priv, is_primary, false);
 
 	mutex_lock(&rz_mtu3_pwm->lock);
 	priv->enable_count--;
