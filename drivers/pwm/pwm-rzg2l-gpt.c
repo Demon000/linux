5388dca1ce2c71a04877f0a7f6449548c2704f6d
Cosmin Tanislav <cosmin-gabriel.tanislav.xa@renesas.com>
2026-06-12 18:22:54 +0300

    pwm: rzg2l-gpt: integrate with core driver
    Change-Id: I88d6ac4af5b45e50391a964b94f67c9bbcb1a926
    Signed-off-by: Cosmin Tanislav <cosmin-gabriel.tanislav.xa@renesas.com>

--- drivers/pwm/pwm-rzg2l-gpt.c
+++ drivers/pwm/pwm-rzg2l-gpt.c
@@ -31,6 +31,7 @@
 #include <linux/clk.h>
 #include <linux/io.h>
 #include <linux/limits.h>
+#include <linux/mfd/rzg2l-gpt.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
@@ -88,7 +89,6 @@
 
 #define RZG2L_GTINTAD_GRP_MASK	GENMASK(25, 24)
 
-#define RZG2L_MAX_HW_CHANNELS	8
 #define RZG2L_CHANNELS_PER_IO	2
 #define RZG2L_MAX_PWM_CHANNELS	(RZG2L_MAX_HW_CHANNELS * RZG2L_CHANNELS_PER_IO)
 #define RZG2L_MAX_SCALE_FACTOR	1024
@@ -97,19 +97,8 @@
 #define RZG2L_MAX_POEG_GROUPS	4
 #define RZG2L_LAST_POEG_GROUP	3
 
-#define RZG2L_INVALID_TPCS	0xff
-
-struct rzg2l_gpt_info {
-	/*
-	 * Maps a prescale to its GTCR.TPCS value. Unsupported prescales must be
-	 * RZG2L_INVALID_TPCS. Last entry must be valid.
-	 */
-	const u8 *prescales;
-	unsigned int num_prescales;
-	u32 gtcr_tpcs;
-};
-
 struct rzg2l_gpt_chip {
+	struct rzg2l_gpt *gpt;
 	void __iomem *mmio;
 	struct mutex lock; /* lock to protect shared channel resources */
 	const struct rzg2l_gpt_info *info;
@@ -553,12 +542,11 @@ err_of_node:
 
 static int rzg2l_gpt_probe(struct platform_device *pdev)
 {
+	struct rzg2l_gpt *ddata = dev_get_drvdata(pdev->dev.parent);
 	struct rzg2l_gpt_chip *rzg2l_gpt;
 	struct device *dev = &pdev->dev;
-	struct reset_control *rstc;
 	struct pwm_chip *chip;
 	unsigned long rate;
-	struct clk *clk;
 	int ret;
 
 	chip = devm_pwmchip_alloc(dev, RZG2L_MAX_PWM_CHANNELS, sizeof(*rzg2l_gpt));
@@ -566,33 +554,15 @@ static int rzg2l_gpt_probe(struct platform_device *pdev)
 		return PTR_ERR(chip);
 	rzg2l_gpt = to_rzg2l_gpt_chip(chip);
 
-	rzg2l_gpt->mmio = devm_platform_ioremap_resource(pdev, 0);
-	if (IS_ERR(rzg2l_gpt->mmio))
-		return PTR_ERR(rzg2l_gpt->mmio);
+	rzg2l_gpt->gpt = ddata;
+	rzg2l_gpt->mmio = ddata->mmio;
+	rzg2l_gpt->info = ddata->info;
 
-	rzg2l_gpt->info = of_device_get_match_data(dev);
-
-	rstc = devm_reset_control_get_exclusive_deasserted(dev, NULL);
-	if (IS_ERR(rstc))
-		return dev_err_probe(dev, PTR_ERR(rstc), "Cannot deassert reset control\n");
-
-	rstc = devm_reset_control_get_optional_exclusive_deasserted(dev, "rst_s");
-	if (IS_ERR(rstc))
-		return dev_err_probe(dev, PTR_ERR(rstc), "Cannot deassert rst_s reset\n");
-
-	clk = devm_clk_get_optional_enabled(dev, "bus");
-	if (IS_ERR(clk))
-		return dev_err_probe(dev, PTR_ERR(clk), "Cannot get bus clock\n");
-
-	clk = devm_clk_get_enabled(dev, NULL);
-	if (IS_ERR(clk))
-		return dev_err_probe(dev, PTR_ERR(clk), "Cannot get clock\n");
-
-	ret = devm_clk_rate_exclusive_get(dev, clk);
+	ret = devm_clk_rate_exclusive_get(dev, ddata->clk);
 	if (ret)
 		return ret;
 
-	rate = clk_get_rate(clk);
+	rate = clk_get_rate(ddata->clk);
 	if (!rate)
 		return dev_err_probe(dev, -EINVAL, "The gpt clk rate is 0\n");
 
@@ -626,57 +596,9 @@ static int rzg2l_gpt_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static const u8 rzg3e_gpt_prescales[] = {
-	[0] = 0x0,
-	[1] = 0x1,
-	[2] = 0x2,
-	[3] = 0x3,
-	[4] = 0x4,
-	[5] = 0x5,
-	[6] = 0x6,
-	[7] = RZG2L_INVALID_TPCS,
-	[8] = 0x8,
-	[9] = RZG2L_INVALID_TPCS,
-	[10] = 0xA,
-};
-
-static const struct rzg2l_gpt_info rzg3e_data = {
-	.prescales = rzg3e_gpt_prescales,
-	.num_prescales = ARRAY_SIZE(rzg3e_gpt_prescales),
-	.gtcr_tpcs = RZG3E_GTCR_TPCS,
-};
-
-static const u8 rzg2l_gpt_prescales[] = {
-	[0] = 0x0,
-	[1] = RZG2L_INVALID_TPCS,
-	[2] = 0x1,
-	[3] = RZG2L_INVALID_TPCS,
-	[4] = 0x2,
-	[5] = RZG2L_INVALID_TPCS,
-	[6] = 0x3,
-	[7] = RZG2L_INVALID_TPCS,
-	[8] = 0x4,
-	[9] = RZG2L_INVALID_TPCS,
-	[10] = 0x5,
-};
-
-static const struct rzg2l_gpt_info rzg2l_data = {
-	.prescales = rzg2l_gpt_prescales,
-	.num_prescales = ARRAY_SIZE(rzg2l_gpt_prescales),
-	.gtcr_tpcs = RZG2L_GTCR_TPCS,
-};
-
-static const struct of_device_id rzg2l_gpt_of_table[] = {
-	{ .compatible = "renesas,r9a09g047-gpt", .data = &rzg3e_data },
-	{ .compatible = "renesas,rzg2l-gpt", .data = &rzg2l_data },
-	{ /* Sentinel */ }
-};
-MODULE_DEVICE_TABLE(of, rzg2l_gpt_of_table);
-
 static struct platform_driver rzg2l_gpt_driver = {
 	.driver = {
 		.name = "pwm-rzg2l-gpt",
-		.of_match_table = rzg2l_gpt_of_table,
 	},
 	.probe = rzg2l_gpt_probe,
 };
@@ -684,4 +606,5 @@ module_platform_driver(rzg2l_gpt_driver);
 
 MODULE_AUTHOR("Biju Das <biju.das.jz@bp.renesas.com>");
 MODULE_DESCRIPTION("Renesas RZ/G2L General PWM Timer (GPT) Driver");
+MODULE_ALIAS("platform:pwm-rzg2l-gpt");
 MODULE_LICENSE("GPL");
